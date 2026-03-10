26706120ab5279014014a403b1c815f51c18f0b7
Tharun Sannapaneni <113322340+Sannapanenitharun@users.noreply.github.com>
2026-03-10 20:30:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "role_arn" {
-  value = aws_iam_role.nidramathu_role.arn
+output "result_entry" {
+  description = "Response from Cloudchipr API Callback function"
+  value       = jsondecode(aws_lambda_invocation.lambda_execution_role_invoke.result)
 }
