359cdd84373b46e1a3073049483e0d0d26d85b0c
Tharun Sannapaneni <113322340+Sannapanenitharun@users.noreply.github.com>
2026-03-10 17:57:26 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "platform_account" {
+  description = "Platform AWS Account ID"
+  type        = string
+}
+
+variable "external_id" {
+  description = "External ID for secure access"
+  type        = string
+}
